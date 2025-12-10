45604911418ed06acc46255a1ce87625fc3fa890
juniorenv <lightgraphics80@gmail.com>
2025-12-10 11:57:10 +0000

    feat(frontend): add About page with placeholder content
    - Create About component with PageLayout wrapper
    - Add placeholder lorem ipsum content for page demonstration
    - Include title "About" for page identification
    - Prepare for navigation integration with react-router

---
+++ src/pages/About.tsx
@@ -0,0 +1,23 @@
+import { PageLayout } from "../shared/layout/page-layout/PageLayout";
+
+export function About() {
+  return (
+    <PageLayout title="About">
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus
+        suscipit blanditiis ad eos vel neque doloribus corporis odit quae. Natus
+        nam fugit dolores ea culpa adipisci ducimus hic commodi dignissimos.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus
+        suscipit blanditiis ad eos vel neque doloribus corporis odit quae. Natus
+        nam fugit dolores ea culpa adipisci ducimus hic commodi dignissimos.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus
+        suscipit blanditiis ad eos vel neque doloribus corporis odit quae. Natus
+        nam fugit dolores ea culpa adipisci ducimus hic commodi dignissimos.
+      </p>
+    </PageLayout>
+  );
+}
